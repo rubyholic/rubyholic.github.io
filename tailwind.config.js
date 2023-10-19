16c644d91254dc5d6e5b6982c60cd89f5623d290
aliterm <me@situsali.com>
2023-10-20 00:28:27 +0700

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ["./_includes/**/*.{html,js}", "./_layouts/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
